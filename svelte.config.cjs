0dd885af2dec2a84091cda39f459cb79e697eb87
Mattias Holmlund <mattias@holmlund.se>
2021-04-30 22:36:49 +0200

    Preprocess markdown

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,10 +1,14 @@
 const preprocess = require('svelte-preprocess');
+const { logger, mdsvDeck } = require('./util/mdsv-processor.cjs');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
+	extensions: ['.svelte', '.md'],
 	preprocess: [
+		mdsvDeck(),
+		logger(),
 		preprocess({
 			postcss: true
 		})
